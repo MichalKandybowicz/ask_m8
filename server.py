4103331b34889c85e3213d0810379c67c24e240e
michalkandybowicz <eejdzent@gmail.com>
2019-03-28 13:55:07 +0100

    Refactored some of the code and added minor new features
    Signed-off-by: michalkandybowicz <eejdzent@gmail.com>

--- server.py
+++ server.py
@@ -18,65 +18,40 @@ def route_list():
 
 @app.route('/add', methods=['GET', 'POST'])
 def route_add_question():
-    edit = False
-    action = '/add'
     if request.method == 'POST':
         data_manager.add_question(request.form)
         return redirect('/list')
-    return render_template('form.html', edit=edit, action=action)
-
-
-@app.route('/question/<question_id>/delete')
-def route_delete_question(question_id):
-    data_manager.delete_element("questions", question_id)
-    return redirect('/list')
-
-
-@app.route('/answer/<combined_id>/delete')
-def route_delete_answer(combined_id):
-    answer_id = combined_id.split('_')[0]
-    question_id = combined_id.split('_')[1]
-    data_manager.delete_element("answers", answer_id)
-    return redirect('/question_detail/' + question_id)
+    return render_template('form.html')
 
 
 @app.route('/question_detail/<id>')
 def route_question_detail(id):
     try:
-        data_manager.question_view_count_increase(id)
         question = data_manager.get_question_by_id(id)
         answers = data_manager.get_answers_by_question_id(id)
-        number_of_answers = len(answers)
-        return render_template('qd.html', question=question, id=id, answers=answers, count=number_of_answers)
+        return render_template('qd.html', question=question, id=id, answers=answers)
     except ValueError:
         return redirect('/')
-
-
-@app.route('/question_detail/<id>/edit', methods=['POST', 'GET'])
-def route_question_edit(id):
-    edit = True
-    action = '/question_detail/' + id + '/edit'
-    question = data_manager.get_question_by_id(id)
-    if request.method == 'POST':
-        data_manager.update_question(id, request.form)
-        return redirect('/question_detail/' + id)
-    return render_template('form.html', edit=edit, question=question, id=id, action=action)
 
 
 @app.route('/question/<id>/new-answer', methods=['GET', 'POST'])
 def route_new_answer(id):
     if request.method == 'POST':
-        data_manager.add_answer(request.form, id)
+        current_file = data_manager.get_all_answers()
+        new_answer = {}
+        if current_file:
+            new_answer['id'] = str(int(current_file[-1]['id']) + 1)
+        else:
+            new_answer['id'] = '0'
+        new_answer['submission_time'] = ''
+        new_answer['vote_number'] = '0'
+        new_answer['question_id'] = id
+        new_answer['message'] = request.form['answer']
+        new_answer['image'] = ''
+        current_file.append(new_answer)
+        data_manager.save_new_answer(current_file)
         return redirect('/question_detail/' + id)
-    return render_template('answer.html', id=id)
-
-
-@app.route('/sort')
-def my_route():
-    feature_to_order_by = request.args.get('order_by', default = 'title', type = str)
-    order_direction = request.args.get('order_direction', default = 'asc', type = str)
-    questions = data_manager.sort_questions(feature_to_order_by, order_direction)
-    return render_template('list.html', questions=questions)
+    return render_template('answer.html')
 
 
 if __name__ == "__main__":
